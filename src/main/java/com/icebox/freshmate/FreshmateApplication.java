0e7fde1f1decec786b8998db6eb0e03487390574
hyee0715 <hyee0715@naver.com>
2023-12-16 01:55:38 +0900

    Initial commit

---
+++ src/main/java/com/icebox/freshmate/FreshmateApplication.java
@@ -0,0 +1,13 @@
+package com.icebox.freshmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreshmateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreshmateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/icebox/freshmate/FreshmateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icebox.freshmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreshmateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
