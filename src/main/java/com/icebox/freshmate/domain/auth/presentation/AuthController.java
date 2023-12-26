b476b09fe3744eb445b29bd01f5bfe437c61dfed
hyee0715 <hyee0715@naver.com>
2023-12-26 17:36:12 +0900

    feat: Access Token 재발급 기능 구현

--- src/main/java/com/icebox/freshmate/domain/auth/presentation/AuthController.java
+++ src/main/java/com/icebox/freshmate/domain/auth/presentation/AuthController.java
@@ -3,13 +3,16 @@ package com.icebox.freshmate.domain.auth.presentation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.icebox.freshmate.domain.auth.application.AuthService;
+import com.icebox.freshmate.domain.auth.application.JwtService;
 import com.icebox.freshmate.domain.auth.application.dto.request.MemberLoginReq;
 import com.icebox.freshmate.domain.auth.application.dto.request.MemberSignUpAuthReq;
 import com.icebox.freshmate.domain.auth.application.dto.request.MemberWithdrawReq;
@@ -25,6 +28,7 @@ import lombok.RequiredArgsConstructor;
 public class AuthController {
 
 	private final AuthService authService;
+	private final JwtService jwtService;
 
 	@PostMapping("/sign-up")
 	public ResponseEntity<MemberInfoRes> signUp(@Valid @RequestBody MemberSignUpAuthReq memberSignUpAuthReq) {
@@ -48,6 +52,18 @@ public class AuthController {
 		return ResponseEntity
 			.noContent()
 			.build();
+	}
+
+	@GetMapping("/reissue")
+	public ResponseEntity<MemberAuthRes> reissueToken(@RequestHeader("Authorization-refresh") String refreshTokenHeader) {
+		String refreshToken = jwtService.getRefreshToken(refreshTokenHeader);
+		String reissuedAccessToken = jwtService.reissueAccessToken(refreshToken);
+
+		MemberAuthRes memberAuthRes = new MemberAuthRes(reissuedAccessToken, refreshToken);
+
+		return ResponseEntity
+			.status(HttpStatus.CREATED)
+			.body(memberAuthRes);
 	}
 
 	@PatchMapping("/logout")
