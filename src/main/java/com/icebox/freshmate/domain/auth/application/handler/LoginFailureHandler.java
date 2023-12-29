0043355abd68449356ca4ae637738ce85e4b4441
hyee0715 <hyee0715@naver.com>
2023-12-30 02:16:18 +0900

    refactor: 로그인 에러 메시지 수정

--- src/main/java/com/icebox/freshmate/domain/auth/application/handler/LoginFailureHandler.java
+++ src/main/java/com/icebox/freshmate/domain/auth/application/handler/LoginFailureHandler.java
@@ -2,8 +2,19 @@ package com.icebox.freshmate.domain.auth.application.handler;
 
 import java.io.IOException;
 
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.CredentialsExpiredException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.icebox.freshmate.global.error.ErrorCode;
+import com.icebox.freshmate.global.error.ErrorResponse;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -14,8 +25,47 @@ public class LoginFailureHandler extends SimpleUrlAuthenticationFailureHandler {
 
 	@Override
 	public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException {
-		response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-		response.getWriter().write("fail");
-		log.info("로그인에 실패했습니다.");
+		ErrorCode errorCode;
+
+		if (exception instanceof BadCredentialsException) {
+			errorCode = ErrorCode.BAD_CREDENTIALS;
+		} else if (exception instanceof InternalAuthenticationServiceException) {
+			errorCode = ErrorCode.BAD_CREDENTIALS;
+		} else if (exception instanceof DisabledException) {
+			errorCode = ErrorCode.DISABLE_ACCOUNT;
+		} else if (exception instanceof CredentialsExpiredException) {
+			errorCode = ErrorCode.CREDENTIALS_EXPIRED;
+		} else if (exception instanceof UsernameNotFoundException) {
+			errorCode = ErrorCode.NOT_FOUND_ACCOUNT;
+		} else if (exception instanceof AuthenticationCredentialsNotFoundException) {
+			errorCode = ErrorCode.NOT_FOUND_AUTHENTICATION_CREDENTIALS;
+		} else {
+			errorCode = ErrorCode.LOGIN_FAILURE;
+		}
+
+		log.info(errorCode.getMessage());
+
+		writeErrorResponse(response, errorCode, HttpServletResponse.SC_BAD_REQUEST);
+	}
+
+	private void writeErrorResponse(HttpServletResponse response, ErrorCode errorCode, int statusCode) throws
+		IOException {
+		String errorResponseJsonFormat = getErrorResponseJsonFormat(errorCode);
+		writeToHttpServletResponse(response, statusCode, errorResponseJsonFormat);
+	}
+
+	private void writeToHttpServletResponse(HttpServletResponse response, int statusCode, String errorMessage) throws
+		IOException {
+		response.setStatus(statusCode);
+		response.setContentType("application/json;charset=UTF-8");
+		response.getWriter().write(errorMessage);
+		response.getWriter().flush();
+		response.getWriter().close();
+	}
+
+	private String getErrorResponseJsonFormat(ErrorCode errorCode) throws JsonProcessingException {
+		ErrorResponse errorResponse = ErrorResponse.of(errorCode);
+		ObjectMapper objectMapper = new ObjectMapper();
+		return objectMapper.writeValueAsString(errorResponse);
 	}
 }
--- src/main/java/com/icebox/freshmate/global/error/ErrorCode.java
+++ src/main/java/com/icebox/freshmate/global/error/ErrorCode.java
@@ -22,6 +22,11 @@ public enum ErrorCode {
 	//인증, 인가
 	INVALID_TOKEN("A001", "토큰의 유효성(형식, 서명 등)이 올바르지 않습니다."),
 	EXPIRED_TOKEN("A002", "토큰이 만료되었습니다."),
+	BAD_CREDENTIALS("A003", "아이디 또는 비밀번호가 일치하지 않습니다."),
+	DISABLE_ACCOUNT("A004", "계정이 비활성화 되었습니다."),
+	CREDENTIALS_EXPIRED("A005", "계정 유효기간이 만료 되었습니다."),
+	NOT_FOUND_ACCOUNT("A006", "계정이 존재하지 않습니다. 회원가입 진행 후 로그인 해주세요."),
+	NOT_FOUND_AUTHENTICATION_CREDENTIALS("A007", "인증 요청이 거부되었습니다."),
 
 	//냉장고
 	NOT_FOUND_ICEBOX("I001", "냉장고가 존재하지 않습니다.");
