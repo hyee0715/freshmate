a8df297f73ca027add751a90c4f2fa7c13833d3a
hyee0715 <hyee0715@naver.com>
2023-12-30 20:38:15 +0900

    chore: Req 오류 메시지 수정

--- src/main/java/com/icebox/freshmate/domain/refrigerator/application/dto/request/RefrigeratorReq.java
+++ src/main/java/com/icebox/freshmate/domain/refrigerator/application/dto/request/RefrigeratorReq.java
@@ -8,7 +8,7 @@ import com.icebox.freshmate.domain.member.domain.Member;
 import jakarta.validation.constraints.NotBlank;
 
 public record RefrigeratorReq(
-	@NotBlank(message = "냉장고 이름을 입력하세요.")
+	@NotBlank(message = "냉장고 이름을 입력해주세요.")
 	@Length(min = 1, max = 50, message = "냉장고 이름은 1자 이상 50자 이하로 등록 가능합니다.")
 	String name
 ) {
