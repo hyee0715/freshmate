61495eb09bc0b40b33a965cf80b20596b3ef59b5
hyee0715 <hyee0715@naver.com>
2024-01-26 03:03:51 +0900

    feat: 식료품 유통기한 정보 알림 기능 구현

--- src/main/java/com/icebox/freshmate/domain/grocery/application/GrocerySchedulingService.java
+++ src/main/java/com/icebox/freshmate/domain/grocery/application/GrocerySchedulingService.java
@@ -4,8 +4,10 @@ import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -14,6 +16,9 @@ import org.springframework.transaction.annotation.Transactional;
 import com.icebox.freshmate.domain.grocery.domain.Grocery;
 import com.icebox.freshmate.domain.grocery.domain.GroceryExpirationType;
 import com.icebox.freshmate.domain.grocery.domain.GroceryRepository;
+import com.icebox.freshmate.domain.notification.application.NotificationEventPublisher;
+import com.icebox.freshmate.domain.notification.application.dto.request.NotificationReq;
+import com.icebox.freshmate.domain.notification.domain.NotificationType;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -23,20 +28,22 @@ import lombok.extern.slf4j.Slf4j;
 public class GrocerySchedulingService {
 
 	private static final String GROCERY_EXPIRATION_UPDATE_PERIOD = "0 10 0 * * *"; //매일 0시 10분에 실행
-	private static final String GROCERY_NOTIFICATION_PERIOD = "0 0 18 * * *"; //매일 18시애 실행
+	private static final String GROCERY_NOTIFICATION_PERIOD = "0 0 18 * * *"; //매일 18시에 실행
 
-	private static final String NOT_EXPIRED_GROCERIES_MESSAGE = "유통기한이 최소 {}일 남은 식료품이 있습니다. 어떤 식료품인지 확인해보세요!";
-	private static final String EXPIRED_GROCERIES_MESSAGE = "유통기한으로부터 최대 {}일 지난 식료품이 있습니다. 어떤 식료품인지 확인해보세요!";
+	private static final String NOT_EXPIRED_GROCERIES_MESSAGE = "유통기한이 최소 %d일 남은 식료품이 있습니다. 어떤 식료품인지 확인해보세요!";
+	private static final String EXPIRED_GROCERIES_MESSAGE = "유통기한으로부터 최대 %d일 지난 식료품이 있습니다. 어떤 식료품인지 확인해보세요!";
 	private static final String TODAY_EXPIRATION_MESSAGE = "오늘이 유통기한 마감일인 식료품이 있습니다. 어떤 식료품인지 확인해보세요!";
 
 	private final GroceryRepository groceryRepository;
 	private final Map<GroceryExpirationType, Consumer<LocalDate>> expirationTypeStrategies;
-	private final Map<Predicate<Integer>, Consumer<Integer>> expirationNotificationStrategies;
+	private final Map<Predicate<Integer>, BiConsumer<Integer, Long>> expirationNotificationStrategies;
+	private final NotificationEventPublisher notificationEventPublisher;
 
-	public GrocerySchedulingService(GroceryRepository groceryRepository) {
+	public GrocerySchedulingService(GroceryRepository groceryRepository, NotificationEventPublisher notificationEventPublisher) {
 		this.groceryRepository = groceryRepository;
 		this.expirationTypeStrategies = initializeExpirationTypeStrategies();
 		this.expirationNotificationStrategies = initializeExpirationStrategies();
+		this.notificationEventPublisher = notificationEventPublisher;
 	}
 
 	@Scheduled(cron = GROCERY_EXPIRATION_UPDATE_PERIOD)
@@ -82,24 +89,35 @@ public class GrocerySchedulingService {
 	}
 
 	private void checkExpirationDate(List<Grocery> groceries, LocalDate currentDate) {
-		if (!groceries.isEmpty()) {
-			Grocery grocery = groceries.get(0);
-			int expirationDate = grocery.calculateExpirationDateFromCurrentDate(currentDate);
 
-			notifyExpirationInformation(expirationDate);
-		}
+		divideGroceriesByMemberId(groceries)
+			.forEach((memberId, groceriesOfMember) -> {
+				int expirationDate = groceriesOfMember.get(0).calculateExpirationDateFromCurrentDate(currentDate);
+				notifyExpirationInformation(expirationDate, memberId);
+			});
 	}
 
-	private void notifyExpirationInformation(int expirationDate) {
+	private Map<Long, List<Grocery>> divideGroceriesByMemberId(List<Grocery> groceries) {
+
+		return groceries.stream()
+			.collect(Collectors.groupingBy(
+				grocery -> grocery.getStorage()
+					.getRefrigerator()
+					.getMember().getId(),
+				Collectors.toList()
+			));
+	}
+
+	private void notifyExpirationInformation(int expirationDate, Long memberId) {
 
 		expirationNotificationStrategies.entrySet().stream()
 			.filter(entry -> entry.getKey().test(expirationDate))
 			.findFirst()
 			.map(Map.Entry::getValue)
-			.ifPresent(strategy -> strategy.accept(expirationDate));
+			.ifPresent(strategy -> strategy.accept(expirationDate, memberId));
 	}
 
-	private Map<Predicate<Integer>, Consumer<Integer>> initializeExpirationStrategies() {
+	private Map<Predicate<Integer>, BiConsumer<Integer, Long>> initializeExpirationStrategies() {
 		return Map.of(
 			days -> days < 0, this::notifyNotExpiredGroceries,
 			days -> days == 0, this::notifyTodayExpirationGroceries,
@@ -107,18 +125,30 @@ public class GrocerySchedulingService {
 		);
 	}
 
-	private void notifyNotExpiredGroceries(int days) {
+	private void notifyNotExpiredGroceries(int days, Long memberId) {
+		String message = String.format(NOT_EXPIRED_GROCERIES_MESSAGE, Math.abs(days));
 
-		log.info(NOT_EXPIRED_GROCERIES_MESSAGE, Math.abs(days));
+		notifyGroceryExpiration(memberId, message);
 	}
 
-	private void notifyTodayExpirationGroceries(int days) {
+	private void notifyTodayExpirationGroceries(int days, Long memberId) {
 
-		log.info(TODAY_EXPIRATION_MESSAGE);
+		notifyGroceryExpiration(memberId, TODAY_EXPIRATION_MESSAGE);
 	}
 
-	private void notifyExpiredGroceries(int days) {
+	private void notifyExpiredGroceries(int days, Long memberId) {
+		String message = String.format(EXPIRED_GROCERIES_MESSAGE, days);
 
-		log.info(EXPIRED_GROCERIES_MESSAGE, days);
+		notifyGroceryExpiration(memberId, message);
+	}
+
+	private void notifyGroceryExpiration(Long memberId, String message) {
+		NotificationReq groceryNotificationReq = getGroceryNotificationReq(memberId, message);
+		notificationEventPublisher.publishEvent(groceryNotificationReq);
+	}
+
+	private NotificationReq getGroceryNotificationReq(Long memberId, String message) {
+
+		return new NotificationReq(memberId, NotificationType.EXPIRATION.name(), message, "");
 	}
 }
