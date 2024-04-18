5d4db9241376c9eb05af364ae8bda8b57f55d48f
hyee0715 <hyee0715@naver.com>
2024-04-18 23:28:36 +0900

    chore: 주석 처리

--- src/main/java/com/icebox/freshmate/global/cicd/HealthCheckController.java
+++ src/main/java/com/icebox/freshmate/global/cicd/HealthCheckController.java
@@ -14,25 +14,25 @@ public class HealthCheckController {
 	@Value("${server.env}")
 	private String env;
 
-	@Value("${server.port}")
-	private String serverPort;
-
-	@Value("${server.serverAddress}")
-	private String serverAddress;
-
-	@Value("${serverName}")
-	private String serverName;
-
-	@GetMapping("/hc")
-	public ResponseEntity<?> healthCheck() {
-		Map<String, String> responseData = new TreeMap<>();
-		responseData.put("serverName", serverName);
-		responseData.put("serverAddress", serverAddress);
-		responseData.put("serverPort", serverPort);
-		responseData.put("env", env);
-
-		return ResponseEntity.ok(responseData);
-	}
+//	@Value("${server.port}")
+//	private String serverPort;
+//
+//	@Value("${server.serverAddress}")
+//	private String serverAddress;
+//
+//	@Value("${serverName}")
+//	private String serverName;
+//
+//	@GetMapping("/hc")
+//	public ResponseEntity<?> healthCheck() {
+//		Map<String, String> responseData = new TreeMap<>();
+//		responseData.put("serverName", serverName);
+//		responseData.put("serverAddress", serverAddress);
+//		responseData.put("serverPort", serverPort);
+//		responseData.put("env", env);
+//
+//		return ResponseEntity.ok(responseData);
+//	}
 
 	@GetMapping("/env")
 	public ResponseEntity<?> getEnv() {
