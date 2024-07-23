08b1afbbd3f0fd076e6b3f76ee62eec418bf874b
hyee0715 <hyee0715@naver.com>
2024-07-23 19:43:10 +0900

    chore: docs 경로 변경

--- src/main/java/com/icebox/freshmate/global/docs/DocsViewController.java
+++ src/main/java/com/icebox/freshmate/global/docs/DocsViewController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class DocsViewController {
 
-	@GetMapping("/docs")
+	@GetMapping("/api/docs")
 	public String getDocs() {
 		return "docs/docs-home";
 	}
